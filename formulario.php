512e4ee17fc187a2c0fa9758e208ff899ebeb41a
brenosantos <brenowillian0357@gmail.com>
2022-05-10 20:15:04 -0300

    formulario php

---
+++ formulario.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="formularioStyle.css">
+</head>
+<body>
+       
+        <!--área do contato----------------->
+        <section id="contato">
+            <div class="container-fluid p-5">
+                <div class="row">
+                    <h2>Fale Conosco</h2>
+                    <form class="" action="cadastro.php" method="POST">
+                        <div class="pt-4">
+                            <input class="" type="text" name="nome" id="" size="40" placeholder="Nome">
+                        </div>
+                        <div class="pt-4">
+                            <input type="email" name="email" id="" size="40" placeholder="E-mail">
+                        </div>
+                        <div class="pt-4">
+                            <textarea name="mensagem" id="" cols="30" rows="10"></textarea>
+                        </div>
+                        <button class="mt-4 btn btn-dark">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+       
+    <!--fim área contato-->
+        
+
+
+</body>
+</html>
